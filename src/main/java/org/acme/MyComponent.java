fde46be07d56ce37528efd54d505ae8600029392
mtrefcon <tnmtechnologies@users.noreply.github.com>
2023-04-03 20:21:12 +0200

    fix javax.enterprise.inject.UnsatisfiedResolutionException: No bean found for required type [class org.acme.MyComponent] and qualifiers [[]]

--- src/main/java/org/acme/MyComponent.java
+++ src/main/java/org/acme/MyComponent.java
@@ -1,11 +1,13 @@
 package org.acme;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 
+@ApplicationScoped
 @Named
 public class MyComponent {
 
-    public String sayHello(String name) {
+    public String sayHello(final String name) {
         return String.format("Hello %s!", name);
     }
 
