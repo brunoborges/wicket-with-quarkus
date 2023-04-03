072bc86673e4d9ab015db20f12eea70773a349f7
mtrefcon <tnmtechnologies@users.noreply.github.com>
2023-04-03 20:10:00 +0200

    fix double init method in WicketApplication

--- src/main/java/com/mycompany/WicketApplication.java
+++ src/main/java/com/mycompany/WicketApplication.java
@@ -1,16 +1,17 @@
 package com.mycompany;
 
-import org.apache.wicket.application.IComponentInstantiationListener;
-import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.protocol.http.WebApplication;
-import org.apache.wicket.Component;
-
-import io.quarkus.runtime.annotations.RegisterForReflection;
+import java.util.Arrays;
+import java.util.logging.Logger;
 
 import javax.enterprise.inject.spi.CDI;
 import javax.inject.Inject;
-import java.util.Arrays;
-import java.util.logging.Logger;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.application.IComponentInstantiationListener;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.protocol.http.WebApplication;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection
 public class WicketApplication extends WebApplication {
@@ -20,16 +21,16 @@ public class WicketApplication extends WebApplication {
         super.init();
         getComponentInstantiationListeners().add(new IComponentInstantiationListener() {
           @Override
-          public void onInstantiation(Component component) {
+          public void onInstantiation(final Component component) {
               Arrays.stream(component.getClass().getDeclaredFields()).forEach(x-> {
                   if(x.isAnnotationPresent(Inject.class)) {
-                      var c = CDI.current().select(x.getType()).get();
-                      boolean canAccess = x.canAccess(component);
+                      final var c = CDI.current().select(x.getType()).get();
+                      final boolean canAccess = x.canAccess(component);
                       x.setAccessible(true);
                       try {
                           x.set(component, c);
                           Logger.getLogger("WicketApplication#init").info("injecting " + component.getClass().getSimpleName() + "#" + x.getName() + "::" + x.getType().getSimpleName());
-                      } catch (IllegalAccessException e) {
+                      } catch (final IllegalAccessException e) {
                           throw new RuntimeException(e);
                       }
                       x.setAccessible(canAccess);
@@ -37,19 +38,13 @@ public class WicketApplication extends WebApplication {
               });
           }
       });
+
+      getCspSettings().blocking().disabled();
 	}
 
 	@Override
 	public Class<? extends WebPage> getHomePage() {
 		return HomePage.class;
-	}
-
-
-	@Override
-	protected void init() {
-	    super.init();
-
-	    getCspSettings().blocking().disabled();
 	}
 
 }
