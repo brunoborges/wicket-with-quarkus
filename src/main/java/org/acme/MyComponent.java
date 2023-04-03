7223550074bdc33ab48add28d2f17a77b9fb0b1e
mtrefcon <tnmtechnologies@users.noreply.github.com>
2023-04-03 20:24:03 +0200

    add logger to test injection

--- src/main/java/org/acme/MyComponent.java
+++ src/main/java/org/acme/MyComponent.java
@@ -1,13 +1,21 @@
 package org.acme;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.jboss.logging.Logger;
 
 @ApplicationScoped
 @Named
 public class MyComponent {
 
+    @Inject
+    Logger logger;
+
     public String sayHello(final String name) {
+        logger.infov("sayHello(name={0})", name);
+
         return String.format("Hello %s!", name);
     }
 
